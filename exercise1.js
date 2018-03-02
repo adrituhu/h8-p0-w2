732f6b768f4f46da0764f12a36f5fa1a20c9f4f9
adrituhu <adrituhu92@gmail.com>
2018-03-02 17:14:31 +0800

    upload tugas exercise1 week 2

---
+++ exercise1.js
@@ -0,0 +1,7 @@
+var massa=prompt ("Masukkan nilai massa");
+massa=parseInt(massa);
+var percepatan=prompt ("Masukkan nilai percepatan");
+percepatan=parseInt(percepatan);
+var resultan;
+resultan=((massa)+(percepatan));
+document.write (" resultan gaya adalah "+resultan);
