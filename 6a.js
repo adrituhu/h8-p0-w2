609be7b4bbd6ab34e07b5e94f0ba62d46c7ae961
adrituhu <36759424+adrituhu@users.noreply.github.com>
2018-03-10 00:51:51 +0800

    update6

--- 6a.js
+++ 6a.js
@@ -1,3 +1,4 @@
+// 1
 var index = 2;
 console.log("looping pertama" );
     while(index <=20){
@@ -11,3 +12,48 @@ console.log("looping kedua" );
       console.log(index + " i love coding " );
       index -=2;
     }
+
+//2
+
+  console.log('LOOPING PERTAMA')
+   for(var i =1 ; i <= 20; i++)
+      {
+    console.log(i +' I Love Coding')
+        }
+    console.log('LOOPING KEDUA')
+      for(var i=20;i>=1; i--)
+        {
+    console.log(i + ' I Will Become Fullstack Developer')
+        }
+//3
+var index =1;
+
+while(index <=40){
+  if(index % 2===0){
+    console.log(index + " genap ");
+  }
+    
+  else {
+    console.log (index + " ganjil ");
+  }
+ index++;
+}
+
+//4
+var index = 1;
+
+while(index <=100 index +=2){
+  if(index % 3 ==0 ){
+    console.log(index + " 3 kelipatan 3 ");
+  }
+    if(index % 6 == index +=5){
+    console.log(index + " 3 kelipatan 3 dan 6 kelipatan 6 ");
+   }
+       if(index % 10 ==0 index +=9 ){
+       console.log(index + " 10 kelipatan 10 ");
+   }
+    else {
+    console.log(index);
+  }
+  index++;
+}
