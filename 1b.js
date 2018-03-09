dfe26f3ca092afb1b37a4f5585f30ce6c692acdf
adrituhu <36759424+adrituhu@users.noreply.github.com>
2018-03-09 23:17:36 +0800

    update 1

--- 1b.js
+++ 1b.js
@@ -1,15 +1,33 @@
-var tahun=prompt ("Masukkan nilai tahun");
-tahun=parseInt(tahun);
- if(tahun % 4 == 0) {
-   if(tahun % 4 == 100) {
-     if(tahun % 4 == 400) {
-        console.log(" tahun " + tahun + " adalah tahun kabisat. ");
-       }
+//Newton Second Law
+READ and SAVE "massa benda"
+READ and SAVE "percepatan benda"
+COMPUTE "massa benda " added by "percepatan benda"
+SAVE previous computation result
+SHOW the computation result
+
+
+
+//Kabisat
+  store tahun with any value
+    
+    if tahun mod 4 = 0 AND tahun mod 100 !== 0
+    display kabisat
+    if tahun mod 4 = 0 AND tahun mod 100 = 0 AND tahun mod 400 = 0
+    display kabisat
+    
+//Laundry
+   store pakaian with any value
+   while pakaian <= 20 
+   do pakaian + 1
+    else
+   display "Cuci pakaian"
+    
+  //Kuku panjang
+  store murid with any value
+   while murid <= 40
+    do murid + 1
+     if murid = kuku panjang
+       then display Hukuman
        else
-      console.log(" tahun " + tahun + " adalah tahun tahun kabisat. ");
-   }
-   else
-  console.log(" tahun " + tahun + " adalah tahun tahun kabisat. ");
-   }
-  else
- console.log(" tahun " + tahun + " adalah bukan tahun kabisat. ");
+       display pujian
+   
